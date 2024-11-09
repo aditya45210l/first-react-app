3e688651f07a99d885d194453b25811a28c2c6c2
aditya45210l <aditya45210l@gmail.com>
2024-11-09 12:16:57 +0530

    adding DarkMode in my TextUtils

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import {useState} from 'react';
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const toggelMode = () =>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      // document.body.style.color = 'white';
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      // document.body.style.color = 'black';
+    }
+  }
   return (
     <>
-  <Navbar titel="TextUtils"/>
+  <Navbar titel="TextUtils" mode={mode} toggelMode={toggelMode}/>
   <div className="container my-3">
-  <TextForm heading="Enter The text to analyze"/>
+  <TextForm heading="Enter The text to analyze" mode={mode}/>
   </div>
   <div>
     {/* <About/> */}
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -35,17 +35,17 @@ export default function Textform(props) {
   const [text, setText] = useState('');
   return (
     <>
-      <div>
+      <div className="container" style={{color : props.mode === 'dark' ? "white":"black"}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-          <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
+          <textarea className="form-control" style={{backgroundColor: props.mode ==='dark'? 'grey':'white',color: props.mode === 'dark' ? 'white': 'black'}} id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
         </div>
         <button className="btn btn-primary mx-1" onClick={hendleUpClick}>Convert to Uppercase</button>
         <button className="btn btn-primary mx-1" onClick={hendleLoClick}>Convert to Lowercase</button>
         <button className="btn btn-primary mx-1" onClick={hendleFirstLetterClick}>Capitalize First Letter</button>
         <button className="btn btn-danger mx-1" onClick={hendleClearClick}>Clear</button>
       </div>
-      <div className="container">
+      <div className="container" style={{color: props.mode === 'dark'? 'white': 'black'}}>
         <h2>Text Summary</h2>
         <p>{text.split(' ').length} words and {text.length} characters</p>
   
